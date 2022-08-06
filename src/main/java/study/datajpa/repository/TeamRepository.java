a6a2799132e3dc2ef2fca5552df8d874c7daa7b8
phs <gustjd0808@pusan.ac.kr>
2022-08-06 22:50:19 +0900

    공통 인터페이스 설정

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,42 +1,9 @@
 package study.datajpa.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-    @PersistenceContext
-    EntityManager em;
-
-    public Team save(Team team){
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team){
-        em.remove(team);
-    }
-
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id){
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count(){
-        return em.createQuery("select count(t) from Team t",Long.class).getSingleResult();
-    }
-
-    public Team find(Long id){
-        return em.find(Team.class,id);
-    }
+// @Repository // 생략 가능, 컴포넌트 스캔을 스프링 데이터 JPA가 자동으로 처리, JPA 예외를 스프링 예외로 변환하는 과정도 자동으로 처리
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
